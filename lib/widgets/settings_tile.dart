f9e3692522f9d1a919d2cc20c0b8cbbe3e96556d
Isaac <isaac9909@gmail.com>
2024-02-16 18:11:02 +0900

    :lipstick: Design: "SettingsScreen UI a bit changed"

--- lib/widgets/settings_tile.dart
+++ lib/widgets/settings_tile.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsTile extends StatelessWidget {
-  SettingsTile({super.key, required this.stIcon, required this.title, required this.goto});
-
+  SettingsTile({super.key, required this.stIcon, required this.title, required this.goto, required this.bgColor});
 
   IconData stIcon;
   String title;
   Widget goto;
+  Color bgColor;
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +18,25 @@ class SettingsTile extends StatelessWidget {
       onTap: () => navigator.push(CupertinoPageRoute(builder: (context) => goto,),),
       child: Row(
         children: [
-          Icon(stIcon),
+          Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.transparent),
+                  color: bgColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                child: Icon(stIcon, color: Colors.white, size: 20,),
+              ),
+            ),
+          ),
+          const SizedBox(width: 15,),
           Text(title, style: const TextStyle(fontWeight: FontWeight.bold),),
         ],
       ),
